9376b0c03bacf6df622f3e1e2a0912e2c25eae6c
DongilMin <tasabi0826@gmail.com>
2025-11-06 20:13:40 +0900

    Feat: Add GIF Animations

--- frontend/src/components/PochitaPage2.jsx
+++ frontend/src/components/PochitaPage2.jsx
@@ -8,40 +8,49 @@ const pochitaPositions = [
   { id: 3, top: '45%', delay: 5 }
 ]
 
-function MovingPochita({ top, delay, id }) {
+const pochita2Positions = [
+  { id: 4, top: '25%', delay: 2 },
+  { id: 5, top: '65%', delay: 8 }
+]
+
+function MovingPochita({ top, delay, id, direction = 'left', gifUrl, altText, duration, fade = true }) {
   const [key, setKey] = useState(0)
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       const interval = setInterval(() => {
         setKey(prev => prev + 1)
-      }, 20000)
+      }, (duration + 7) * 1000)
       return () => clearInterval(interval)
     }, delay * 1000)
 
     return () => clearTimeout(timeout)
-  }, [delay])
+  }, [delay, duration])
+
+  const isRightToLeft = direction === 'left'
+  const opacityAnimation = fade ? [0, 1, 1, 0] : 1
+  const times = fade ? [0, 0.05, 0.95, 1] : undefined
 
   return (
     <motion.div
       key={`${id}-${key}`}
       className="fixed z-20 pointer-events-none"
       style={{ top }}
-      initial={{ left: 'calc(100% + 200px)', opacity: 0 }}
+      initial={{ left: isRightToLeft ? 'calc(100% + 200px)' : '-200px', opacity: fade ? 0 : 1 }}
       animate={{
-        left: '-200px',
-        opacity: [0, 1, 1, 0]
+        left: isRightToLeft ? '-200px' : 'calc(100% + 200px)',
+        opacity: opacityAnimation
       }}
       transition={{
-        duration: 13,
-        times: [0, 0.05, 0.95, 1],
+        duration: duration,
+        times: times,
         ease: 'linear',
         delay: key === 0 ? delay : 0
       }}
     >
       <img
-        src="https://d3ro18w755ioec.cloudfront.net/assets/pochita-unscreen.gif"
-        alt="Pochita"
+        src={gifUrl}
+        alt={altText}
         className="h-48 w-auto"
       />
     </motion.div>
@@ -57,6 +66,22 @@ export default function PochitaPage2() {
           id={pos.id}
           top={pos.top}
           delay={pos.delay}
+          gifUrl="https://d3ro18w755ioec.cloudfront.net/assets/pochita-unscreen.gif"
+          altText="Pochita"
+          duration={13}
+        />
+      ))}
+      {pochita2Positions.map((pos) => (
+        <MovingPochita
+          key={pos.id}
+          id={pos.id}
+          top={pos.top}
+          delay={pos.delay}
+          direction="left"
+          gifUrl="https://d3ro18w755ioec.cloudfront.net/assets/pochita2.gif"
+          altText="Pochita 2"
+          duration={9}
+
         />
       ))}
     </>
